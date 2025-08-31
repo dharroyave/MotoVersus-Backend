4d5d9dd65d68827fd17f96109b6b945b03d72b8e
nicolcortes-angel <nicolmurciacortes7@gmail.com>
2025-08-31 17:59:44 -0500

    Actualizacionde modelos

---
+++ models/categorias.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose";
+
+
+const categoriaSchema = new mongoose.Schema({
+
+ titulo: {
+    tyoe:String,
+    required: true
+ },
+
+ descripcion: {
+    type:String,
+ },
+
+}); 
+
+export const categoriaModel = mongoose.model("Categorias", categoriaSchemaSchema);
---
+++ models/users.js
@@ -0,0 +1,40 @@
+
+import mongoose from "mongoose";
+
+
+const usuarioSchema = new mongoose.Schema({
+    
+ nombre:{
+    type:String, // el tipo de dato la primera en mayusucla
+    required: true
+},
+ Apellido: {
+    tyoe:String,
+    required: true
+ },
+
+ User: {
+    type:String,
+     required: true
+ },
+
+contraseña:{
+    type:String,
+     required: true
+},
+
+correo: {
+    type:String,
+     required: true
+
+},
+
+numero:{
+    type:Number,
+    required: true
+},
+
+
+}); 
+
+export const Modelodeusuario = mongoose.model("Usuarios", usuarioSchema);

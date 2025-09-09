94003742f40e31ece0878534d02c6a9ec29194fc
Alexander Rippe <duwar95@gmail.com>
2025-09-08 21:24:15 -0500

    [controller y routes creados de comentarios]

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { conexionMongo } from './src/config/db.js'; 
+import {commentRouter} from './src/routes/comments.routes.js';
 
 // 2. Importamos la configuración de la base de datos
 const app = express();
@@ -13,6 +14,9 @@ conexionMongo(); // Llamamos a la función para conectar a la base de datos
 app.get('/', (req, res) => {
     res.send('Bienvenido a MotoVersus');
 });
+
+app.use(express.json());
+app.use('/comments', commentRouter);
 
 //4. Levantamos el servidor
 app.listen(PORT, () => {
---
+++ src/controllers/comments.controller.js
@@ -0,0 +1,69 @@
+
+//1. import dependencias y modulos necesarios
+import { comentarioModel } from "../models/comments.model.js";
+
+//Definir las aciones que van a realizar - CRUD
+
+//1. Metodo para Crear un producto -> POST
+export const postProduct = async (req, res) => {
+    try {
+        await comentarioModel.create(req.body);
+        return res.status(201).json({
+            "mensaje": "Comentario creado correctamente!"
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            "mensaje": "Ocurrio un error al crear el comentario!",
+            "error": error.message || error
+        })
+    }
+};
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const allProducts = await comentarioModel.find();
+        return res.status(200).json({
+            "mensaje": "Se encontraron todos los comentarios!",
+            "data": allProducts
+        });
+    } catch (error) {
+        return res.status(500).json({
+            "mensaje": "Ocurrio un error al mostrar los comentarios!",
+            "error": error.message || error
+        })
+    }
+};
+
+export const putProductById = async (req, res) => {
+      try {
+        const idForUpdate = req.params.id;
+        const dataForUpdate = req.body;
+        await comentarioModel.findByIdAndUpdate(idForUpdate,dataForUpdate);
+        return res.status(200).json({
+            "mensaje": "Comentario actualizado correctamente!"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            "mensaje": "Ocurrio un error al actualizar el comentario!",
+            "error": error.message || error
+        })
+    }
+}
+
+export const deleteProductById = async(req, res) => {
+      try {
+        const idForDelet = req.params.id;
+        await comentarioModel.findByIdAndDelete(idForDelet);
+        return res.status(200).json({
+            "mensaje": "Comentario eliminado correctamente!"
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            "mensaje": "Ocurrio un error al eliminar el comentario!",
+            "error": error.message || error
+        })
+    }
+}
